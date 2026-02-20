2995f612e929488a78d7c392df3cdb2761f5ae07
Sebastian Pietras <me@spietras.dev>
2026-02-20 22:21:11 +0100

    Fetched template changes

--- src/loris/models/events/event.py
+++
@@ -1,8 +0,0 @@
-from typing import Annotated
-
-from pydantic import Field
-
-from loris.models.events import bar as be
-from loris.models.events import foo as fe
-
-type Event = Annotated[be.BarEvent | fe.FooEvent, Field(discriminator="type")]
